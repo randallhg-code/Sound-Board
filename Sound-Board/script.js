4ec091aefde291d8fde8821a0632608dfe41c48b
Hannah Randall <hannahrandall@hannahs-mbp.mynetworksettings.com>
2026-02-13 22:36:30 -0500

    Updated music player

--- Sound-Board/script.js
+++ Sound-Board/script.js
@@ -4,7 +4,7 @@ const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong']
 //for each sound create a button, add a class of 'btn', update the text
 sounds.forEach((sound) => {
     //create a button
-    const btn = document.createElement('button')
+    const btn = document.createElement('button');
 
     //add a class of 'btn' to the button element
     btn.classList.add('btn');
@@ -12,6 +12,20 @@ sounds.forEach((sound) => {
     //set the button label text to be the sound name
     btn.innerText = sound;
 
+    //add an event listener to this button
+    btn.addEventListener('click', () => {
+        stopSounds();
+        document.getElementById(sound).play();
+    })
     // add the button to the DOM
     document.getElementById('buttons').appendChild(btn);
+
 });
+
+function stopSounds() {
+    sounds.forEach((sound) => {
+        const snd = document.getElementById(sound);
+        snd.pause();
+        snd.currentTime = 0;
+    })
+}
